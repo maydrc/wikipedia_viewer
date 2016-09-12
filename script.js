a5ecdd21b9d236a48d08f0cbad5555c9a49eec9d
Dulce Rodriguez <maydrc@gmail.com>
2016-09-11 22:39:08 -0700

    updates

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
+var originalCard = $('.card').clone();
+
 function callApi(search) {
+  $('#wiki-results').empty();
   $('.button-positioning').addClass('up');
   $('.noReturn-formating').addClass('hidden');
   $.getJSON('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cpageimages%7Cinfo&generator=search&exsentences=1&exlimit=20&exintro=1&explaintext=1&piprop=original&pilimit=50&inprop=url&gsrsearch=' + search + '&callback=?', function(data) {
     console.log(data);
     if (data.query && data.query.pages) {
-
+      $('.overlayOff').addClass('overlayOn');
+      for (var key in data.query.pages) {
+        if (data.query.pages.hasOwnProperty(key)) {
+          var obj = data.query.pages[key];
+          var copiedCard = originalCard.clone();
+          $(copiedCard).attr('href', obj.fullurl);
+          $(copiedCard).find('.cardTitle').text(obj.title);
+          $(copiedCard).removeClass('hidden');
+          $(copiedCard).appendTo('#wiki-results');
+          $(copiedCard).on('mouseover', { // Passing data to the handler (closure)
+            extract: obj.extract
+          }, function(event) {
+            if (event.data.extract) {
+              $(this).find('.cardTitle').addClass('extracted-text').text(event.data.extract);
+            }
+            $('.card').addClass('opacityCard');
+            $(this).removeClass('opacityCard');
+          });
+          $(copiedCard).on('mouseleave', {
+            title: obj.title
+          }, function(event) {
+            $(this).find('.cardTitle').removeClass('extracted-text').text(event.data.title);
+            $('.card').removeClass('opacityCard');
+          });
+        }
+      }
     } else {
+      $('.overlayOff').addClass('overlayOn');
       $('.noReturn-formating').removeClass('hidden');
     }
   });
+}
+function clear() {
+  $('#search-input').removeClass('open');
+  $('#wiki-results').empty();
+  $('.overlayOff').removeClass('overlayOn');
+  $('body').addClass('webpage-background');
+  $('.noReturn-formating').addClass('hidden');
 }
 $(document).ready(function() {
   $('#search-button').on('click', function() {
     var search = $('#search-input').val();
     if ($('#search-input').hasClass('open') && search === '') {
-      $('#search-input').removeClass('open');
+      clear();
     } else if ($('#search-input').hasClass('open') && search !== '') {
       callApi(search);
     } else {
@@ -27,7 +63,7 @@ $(document).ready(function() {
       callApi(search);
     }
     if (event.keyCode === 13 && search === '') {
-      $('#search-input').removeClass('open');
+      clear();
       $('#search-button').focus();
     }
   });
@@ -36,7 +72,7 @@ $(document).ready(function() {
       var search = $('#search-input').val();
       var hasFocus = $('#search-button').is(':focus');
       if (search === '' && !hasFocus) {
-          $('#search-input').removeClass('open');
+        clear();
       }
     }, 100);
   });
